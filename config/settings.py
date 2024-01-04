24e5b02c19f5f5ee6b1ab778208527baadaab3cb
MGray2 <mgray24@basecampcodingacademy.org>
2024-01-04 11:06:20 -0600

    fixed widget tweeks error

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app",  # already installed
+    "widget_tweaks",
+    "app",
 ]
 
 MIDDLEWARE = [
